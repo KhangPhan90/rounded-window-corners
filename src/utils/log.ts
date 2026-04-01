fd904a1573a6541b6542007502a260bf2c3a307f
KhangPhan90 <KhangPhan90@users.noreply.github.com>
2026-04-01 19:15:10 +0700

    chore: rename log prefix for Next

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -3,18 +3,18 @@
 import {getPref} from './settings.js';
 
 /**
- * Log a message with a [Rounded Window Corners] prefix, but only
+ * Log a message with a [Rounded Window Corners Next] prefix, but only
  * when debug mode is enabled.
  */
 export function logDebug(...args: unknown[]) {
     if (getPref('debug-mode')) {
-        console.log(`[Rounded Window Corners] ${args}`);
+        console.log(`[Rounded Window Corners Next] ${args}`);
     }
 }
 
 /**
- * Log an error with a [Rounded Window Corners] prefix.
+ * Log an error with a [Rounded Window Corners Next] prefix.
  */
 export function logError(...args: unknown[]) {
-    console.error(`[Rounded Window Corners] ${args}`);
+    console.error(`[Rounded Window Corners Next] ${args}`);
 }
